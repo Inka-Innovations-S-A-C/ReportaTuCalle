c2bd9cef46d95f366bab8214892dc80ce7bf66c3
PaulLeon10 <paul.leon.g@uni.pe>
2026-06-04 21:56:55 -0400

    Eliminando codigo obsoleto

--- backend/src/main/java/com/reportatucalle/ReportaTuCalleApp.java
+++ backend/src/main/java/com/reportatucalle/ReportaTuCalleApp.java
@@ -3,6 +3,14 @@ package com.reportatucalle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/* 
+  Punto de entrada principal de la aplicación ReportaTuCalle. Esta clase se encarga de iniciar la aplicación Spring Boot.
+  Arquitectura: Monolito Modular que a futuro puede ser refactorizado a microservicios.
+  Actualmente, todas las funcionalidades están integradas en una sola aplicación,
+  pero se han diseñado módulos separados para facilitar la transición a microservicios en el futuro.
+*/
+
+
 @SpringBootApplication
 public class ReportaTuCalleApp {
     public static void main(String[] args) {
